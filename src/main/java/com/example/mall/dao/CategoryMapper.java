4e4b6a17e2b4318433ecbc3ecceaef3a8513d385
武坤 <kun.wu@tianrang-inc.com>
2019-11-19 16:10:41 +0800

    MyBatis 注解

---
+++ src/main/java/com/example/mall/dao/CategoryMapper.java
@@ -0,0 +1,13 @@
+package com.example.mall.dao;
+
+import com.example.mall.pojo.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+//@Mapper
+public interface CategoryMapper {
+
+    @Select("select * from mall_category where id = #{id}")
+    Category findById(@Param("id") Integer id);
+}
