fb2afa26d3f0c2009d03aca746454d524db1363f
武坤 <kun.wu@tianrang-inc.com>
2019-12-26 11:49:22 +0800

    user login unit test

--- src/test/java/com/example/mall/MallApplicationTests.java
+++ src/test/java/com/example/mall/MallApplicationTests.java
@@ -9,4 +9,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallApplicationTests {
+
+    // 随便加一个测试方法，避免maven打包时报下面的错误
+    // [ERROR] Errors:
+    // [ERROR]   MallApplicationTests.initializationError »  No runnable methods
+    @Test
+    public void a() {
+
+    }
 }
--- src/test/java/com/example/mall/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/mall/service/impl/UserServiceImplTest.java
@@ -1,9 +1,14 @@
 package com.example.mall.service.impl;
 
 import com.example.mall.MallApplicationTests;
+import com.example.mall.enums.ResponseEnum;
 import com.example.mall.enums.RoleEnum;
 import com.example.mall.pojo.User;
 import com.example.mall.service.IUserService;
+import com.example.mall.vo.ResponseVo;
+import lombok.val;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,13 +18,23 @@ import static org.junit.Assert.*;
 @Transactional
 public class UserServiceImplTest extends MallApplicationTests {
 
+    public static final String USERNAME = "Jack";
+    public static final String PASSWORD = "123456";
+
     @Autowired
     private IUserService userService;
 
-    @Test
+//    @Test
+    @Before
     public void register() {
-
-        User user = new User("Jack", "123456", "jack@qq.com", RoleEnum.CUSTOMER.getCode());
+        User user = new User(USERNAME, PASSWORD, "jack@qq.com", RoleEnum.CUSTOMER.getCode());
         userService.register(user);
+    }
+
+    @Test
+    public void login() {
+//        register();
+        ResponseVo responseVo = userService.login(USERNAME, PASSWORD);
+        Assert.assertEquals(ResponseEnum.SUCCESS.getCode(), responseVo.getStatus());
     }
 }
